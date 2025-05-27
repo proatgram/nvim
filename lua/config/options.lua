3d6ffe2efd83f360431648ba00841eeec7afd6c6
Timothy Hutchins <tgh6407@gmail.com>
2025-05-27 10:26:58 -0500

    Modified yuckls and added clipboard support

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,4 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.number = true
+vim.opt.clipboard = "unnamedplus"
--- lua/config/yuckls.lua
+++ lua/config/yuckls.lua
@@ -7,8 +7,7 @@ vim.api.nvim_create_autocmd(
             print(string.format("starting yuck;s for %s", vim.inspect(event)))
             vim.lsp.start {
                 name = "YuckLs",
-                cmd = {"dotnet", "/home/gitrepos/yuckls/YuckLS/dist/YuckLS.dll"}, --this must be where you cloned this repo to.
-                --cmd = {"yuckls"} -- if installed from aur
+                    cmd = {"yuckls"}, -- if installed from aur
                 root_dir = vim.fn.getcwd()
             }
         end
